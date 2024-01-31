c2e50b3d2c787b49c33e6cb36b64529a585faabb
igordt <i.ditota@studenti.unipi.it>
2024-01-31 15:57:43 +0100

    initial import of prep() function
    Questa versione è l'ultima che funziona, fino al sampling. Le trasformazioni di preprocessing vengono effettuate dal funzione prep().

--- trasformation.py
+++ trasformation.py
@@ -1,31 +1,25 @@
 import numpy as np
 from sklearn import preprocessing
 
-def prep(original,input, dir, index_log, range_quantile, n_quantiles):
+def prep(original,transformed, dir, index_log, range_quantile, n_quantiles):
+
+    y = np.copy(original)
+
+    for i in index_log:
+        y[:,i] = np.log(10 + y[:,i])
     
+    scaler = preprocessing.StandardScaler().fit(y)
+    y = scaler.transform(y)
+
+    quantile = preprocessing.QuantileTransformer(n_quantiles=n_quantiles,output_distribution='normal').fit(y[:,min(range_quantile):max(range_quantile)+1])
+    y[:,min(range_quantile):max(range_quantile)+1] = quantile.transform(y[:,min(range_quantile):max(range_quantile)+1])
+
     if dir == 'forward':
-        
-        for i in index_log:
-            input[:,i] = np.log(10 + input[:,i])
-        
-        scaler = preprocessing.StandardScaler().fit(input)
-        input = scaler.transform(input)
+        return y
 
-        quantile = preprocessing.QuantileTransformer(n_quantiles=n_quantiles,output_distribution='normal').fit(input[range_quantile])
-        for j in range_quantile:
-            print(j)
-            input[:,j] = quantile.transform(input[:,j])
-    
-    elif dir == 'backward':
-        quantile = preprocessing.QuantileTransformer(n_quantiles=n_quantiles,output_distribution='normal').fit(original[range_quantile])
-        for j in range_quantile:
-            print(j)
-            input[:,j] = quantile.inverse_transform(input[:,j])
-        
-        scaler = preprocessing.StandardScaler().fit(original)
-        input = scaler.inverse_transform(input)
-        
+    if dir == 'backward':
+        transformed[:,min(range_quantile):max(range_quantile)+1] = quantile.inverse_transform(transformed[:,min(range_quantile):max(range_quantile)+1])
+        transformed = scaler.inverse_transform(transformed)
         for i in index_log:
-            input[:,i] = np.exp(input[:,i]) - 10
-
-    return input    
+            transformed[:,i] = np.exp(transformed[:,i]) - 10
+        return transformed
