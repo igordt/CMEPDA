2bc71e536355eb8b7013c1ae689c917aa6811da7
igordt <i.ditota@studenti.unipi.it>
2024-03-04 17:23:06 +0100

    added comments for documentation

--- utils/model.py
+++ utils/model.py
@@ -1,3 +1,28 @@
+"""
+This file contains the definition of the model and related functions for training and optimization.
+
+The model_definer function defines a flow-based model using the nflows library. It takes the following parameters:
+- num_features: The number of input features for the model.
+- num_iterations: The number of base distributions to generate the flow.
+- hidden_features: The number of hidden features in the MaskedAffineAutoregressiveTransform.
+- patience: The number of epochs with no improvement after which the learning rate will be reduced.
+- factor: The factor by which the learning rate will be reduced.
+- min_lr: The minimum learning rate.
+- initial_lr: The initial learning rate.
+
+The function returns the following objects:
+- flow: The flow-based model.
+- optimizer: The optimizer used for training the model.
+- scheduler: The learning rate scheduler.
+
+The model is defined using a base distribution (StandardNormal) and a series of transformations. The transformations include
+RandomPermutation and MaskedAffineAutoregressiveTransform. The CompositeTransform combines all the transformations.
+
+The model is trained using the Adam optimizer and the learning rate is reduced using the ReduceLROnPlateau scheduler.
+
+The function also prints the number of iterations, number of hidden features, and number of trainable parameters in the model.
+"""
+
 from nflows import transforms
 from nflows.distributions import StandardNormal
 from nflows.flows import Flow
@@ -7,11 +32,24 @@ from nflows.transforms import RandomPermutation
 from nflows.transforms import MaskedAffineAutoregressiveTransform
 from nflows.transforms import CompositeTransform
 
-
-
-
 def model_definer(num_features, num_iterations, hidden_features, patience, factor, min_lr, initial_lr):
+    """
+    Define a flow-based model using the nflows library.
 
+    Args:
+        num_features : The number of input features for the model.
+        num_iterations : The number of iterations to perform in the flow.
+        hidden_features : The number of hidden features in the MaskedAffineAutoregressiveTransform.
+        patience : The number of epochs with no improvement after which the learning rate will be reduced.
+        factor : The factor by which the learning rate will be reduced.
+        min_lr : The minimum learning rate.
+        initial_lr : The initial learning rate.
+
+    Returns:
+        flow : The flow-based model.
+        optimizer : The optimizer used for training the model.
+        scheduler : The learning rate scheduler.
+    """
     base_dist = StandardNormal(shape=[num_features])
 
     transforms = []
--- utils/our_classes.py
+++ utils/our_classes.py
@@ -6,10 +6,30 @@ import torch
 
 
 class Preprocessor:
-    '''
-    Description: 
-    '''
+    """
+    A class that performs data preprocessing.
+
+    Attributes:
+        index_log : List of indices of columns to apply logarithmic transformation.
+        range_quantile : Range of columns to apply quantile transformation.
+        n_quantiles : Number of quantiles to use for quantile transformation.
+        standard_scaler : StandardScaler object for standardization.
+        quantile_scaler : QuantileTransformer object for quantile transformation.
+
+    Methods:
+        forward(data): Preprocesses the input data.
+        backward(data_reconstructed_preprocessed): Reconstructs the preprocessed data.
+
+    """
+
     def __init__(self, settings):
+        """
+        Initializes the Preprocessor object.
+
+        Args:
+            settings : Dictionary containing the preprocessing settings.
+
+        """
         self.index_log = settings['index_log']
         self.range_quantile = settings['range_quantile']
         self.n_quantiles = settings['n_quantiles']
@@ -17,6 +37,16 @@ class Preprocessor:
         self.quantile_scaler = preprocessing.QuantileTransformer(n_quantiles=self.n_quantiles,output_distribution='normal')
 
     def forward(self,data):
+        """
+        Preprocesses the input data.
+
+        Args:
+            data : Input data to be preprocessed.
+
+        Returns:
+            numpy.ndarray: Preprocessed data.
+
+        """
         y = np.copy(data)
 
         for i in self.index_log:
@@ -33,6 +63,16 @@ class Preprocessor:
         return data_preprocessed
     
     def backward(self, data_reconstructed_preprocessed):
+        """
+        Reconstructs the preprocessed data.
+
+        Args:
+            data_reconstructed_preprocessed : Preprocessed data to be reconstructed.
+
+        Returns:
+            numpy.ndarray: Reconstructed data.
+
+        """
         data_reconstructed = np.copy(data_reconstructed_preprocessed)
         if self.range_quantile:
             data_reconstructed[:,min(self.range_quantile):max(self.range_quantile)+1] = self.quantile_scaler.inverse_transform(data_reconstructed[:,min(self.range_quantile):max(self.range_quantile)+1])
@@ -45,19 +85,47 @@ class Preprocessor:
 
 
 class Compressor:
-    '''
-    Description: 
-    '''
+    """
+    A class that performs data compression using a flow model.
+
+    Attributes:
+        flow : Flow model for data compression.
+        N : Number of bits used for compression.
+        limit : Limit value for scaling the compressed data.
+        maxabsscaler : MinMaxScaler object for scaling.
+
+    Methods:
+        compress(data): Compresses the input data.
+        decompress(data_compressed): Decompresses the compressed data.
+
+    """
+
     def __init__(self, flow, N, limit):
+        """
+        Initializes the Compressor object.
+
+        Args:
+            flow : Flow model for data compression.
+            N : Number of bits used for compression.
+            limit : Limit value for scaling the compressed data.
+
+        """
         self.flow = flow
         self.N = N
         self.limit = limit
         self.maxabsscaler = preprocessing.MinMaxScaler()
         
     def compress(self,data):
-        '''
-        Con questo if guardiamo se data è già su cuda, se non lo è lo mettiamo su cuda
-        '''
+        """
+        Compresses the input data.
+
+        Args:
+            data : Input data to be compressed.
+
+        Returns:
+            tuple: A tuple containing the compressed data, the gaussian distribution, and the uniform distribution.
+
+        """
         if isinstance(data, np.ndarray):
             data = torch.tensor(data).to('cuda').float()
     
@@ -75,6 +143,16 @@ class Compressor:
         return data_compressed, gaus, unif
     
     def decompress(self,data_compressed):
+        """
+        Decompresses the compressed data.
+
+        Args:
+            data_compressed : Compressed data to be decompressed.
+
+        Returns:
+            tuple: A tuple containing the decompressed data and the gaussian distribution after erfinv.
+
+        """
         unif = data_compressed/2**self.N
         gaus = scipy.special.erfinv(unif)
 
@@ -86,4 +164,3 @@ class Compressor:
         data_decompressed = data_tensor_decompressed.cpu().detach().numpy()
 
         return data_decompressed, gaus
-    
